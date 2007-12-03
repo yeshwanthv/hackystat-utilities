d2ce0089b1d51fa0915cfea08b78ad8a0ce4eb9a
philipmjohnson <philipmjohnson@3b555386-8a34-0410-ba6b-4dfd32940ad1>
2007-12-03 00:02:54 +0000

    [Issue 14] Provide HackystatHome.

---
+++ trunk/src/org/hackystat/utilities/home/HackystatUserHome.java
@@ -0,0 +1,24 @@
+package org.hackystat.utilities.home;
+
+import java.io.File;
+
+/**
+ * Provides a utility that returns the desired location of the .hackystat directory. 
+ * This defaults to the user.home System Property, but can be overridden by the user
+ * by providing the property hackystat.user.home.
+ * @author Philip Johnson
+ *
+ */
+public class HackystatUserHome {
+  
+  /**
+   * Return a File instance representing the desired location of the .hackystat directory.
+   * Note that this directory may or may not exist. 
+   * @return A File instance representing the desired user.home directory. 
+   */
+  public static File getHome() {
+    String userHome = System.getProperty("hackystat.user.home", System.getProperty("user.home"));
+    return new File(userHome);
+  }
+
+}
